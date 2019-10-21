fddccbfc9befcf37a7577981fc911d2e7b83e80b
Dominik <bruno481995@gmail.com>
2019-10-21 19:42:46 +0200

    initial

---
+++ Aufgabe1_Lauflicht_in_Assembler/Aufgabe1_Lauflicht_in_Assembler/main.asm
@@ -0,0 +1,72 @@
+;
+; Aufgabe1_Lauflicht_in_Assembler.asm
+;
+; Created: 12.10.2019 11:31:22
+; Author : Bruno
+;
+
+
+.INCLUDE <m328pdef.inc>; Controller Konstanten (z.B. RAMEND) 
+.CSEG; Hinweis an Assembler hier kommt Code 
+
+; Replace with your application code
+
+.ORG 0x000
+rjmp START; Bei reset springe nach adresse start
+
+.ORG INT_VECTORS_SIZE
+
+start: ;10min
+
+	ldi r16, high(RAMEND); RAMEND definiert in m328pdef.inc 
+	out SPH, r16; Oberes Byte des Stackpointer setzen 
+	ldi r16, low(RAMEND) 
+	out SPL,r16; Unteres Byte des Stackpointer setzen 
+
+	ldi r16,0b00011100
+	out DDRD, r16
+	
+
+main: ;20 min 
+
+	sbi PortD, 3; SBI kann direkt an der stelle den Port setzen 
+	rcall delay
+	cbi PortD, 3
+	sbi PortD, 4
+	rcall delay
+	cbi PortD, 4
+	sbi PortD, 5
+	rcall delay
+	cbi PortD, 5
+	sbi PortD, 4
+	rcall delay
+	cbi PortD, 4
+
+	rjmp main
+
+
+delay: ;30min 
+
+	ldi r17, 16
+loop0:
+	ldi r18, 255
+	rcall loop1
+	dec r17
+	brne loop0
+	ret
+loop1:
+	ldi r19, 255
+	rcall loop2
+	dec r18
+	brne loop1
+	ret
+loop2:
+	dec r19
+	brne loop2
+	ret
+	
+	
+
+
+
+    
---
+++ Aufgabe2/Aufgabe2/main.c
@@ -0,0 +1,58 @@
+#include <avr/io.h>
+#define F_CPU 16000000UL
+#include	<util/delay.h>
+
+void led_off(int led);
+void led_on (int led);
+
+int main (void)
+{
+	DDRD |= (1 << PD3 | 1 << PD4 | 1 << PD5);
+	
+	while(1)
+	{
+		//PORTB ^= (1 << PB0);
+		//_delay_ms(200);
+		
+		int i;
+		for(i = 0; i < 3; i++)
+		{
+			led_on(i);
+			_delay_ms(200);
+			led_off(i);
+		}
+		
+		//i = 2;
+		
+		for(i = 1; i > 0; i--)
+		{
+			led_on(i);
+			_delay_ms(200);
+			led_off(i);
+		}
+	}
+	return 0;
+}
+
+void led_on (int led)
+{
+	//PORTD.bit(led) = 1
+	
+	//PORTD ^= (1 << PD(led));
+	
+	if(led == 0){PORTD = (1 << PD3);}
+	else if(led == 1){PORTD = (1 << PD4);}
+	else if(led == 2){PORTD = (1 << PD5);}
+}
+
+void led_off(int led)
+{
+	//PORTD.bit(led) = 0
+	
+	//PORTD ^= (0 << PD(led));
+	
+	if(led == 2){PORTD = (0 << PD5);}
+	else if(led == 1){PORTD = (0 << PD4);}
+	else if(led == 0){PORTD = (0 << PD3);}
+}
+
