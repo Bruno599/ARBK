297207d551b4c8a6b49b5a439bf9cba496aab787
Dominik <bruno481995@gmail.com>
2019-11-20 14:20:11 +0100

    a4

--- Aufgabe4/Aufgabe4/main.c
+++ Aufgabe4/Aufgabe4/main.c
@@ -6,13 +6,79 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
+volatile unsigned int millisekunden;
+volatile unsigned int sekunde;
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	DDRD |= (1 << PD3 | 1 << PD4 | 1 << PD5);
+	
+	  // Timer 0 konfigurieren
+  
+	  TCCR0A = (1<<WGM01); // CTC Modus
+	  TCCR0B |= (1<<CS01); // Prescaler 8 //setzt timer in betrieb 
+  
+	  // ((1000000/8)*0,001)-1 = 124
+	  OCR0A = 124;
+
+	  // Compare Interrupt erlauben
+	  TIMSK |= (1<<OCIE0A);
+
+	  // Global Interrupts aktivieren
+	  sei();
+
+	  while(1)
+	  {
+		waitUntil(5000);
+		
+		PORTD = (1 << PD4);
+		
+		waitFor(10000);
+		
+		PORTD = (0 << PD4);
+		
+		waitUntil(5000);
+	  }
 }
 
+waitFor(int32_t ms)
+{
+	unsigned int wait = millisekunden + ms;
+	while(wait >= millisekunden)
+	{
+		PORTD = (1 << PD3);
+	}
+	PORTD = (0 << PD3);
+}
+
+waitUntil(int32_t ms)
+{
+	while(millisekunden <= ms)
+	{
+		PORTD = (1 << PD5);
+	}
+	PORTD = (0 << PD5);
+}
+
+#ifndef TIMER0_OVF_vect
+
+ISR (TIMER0_OVF_vect)
+{
+  /*
+  Der Compare Interrupt Handler
+  wird aufgerufen, wenn
+  TCNT0 = OCR0A = 125-1
+  heißt nach 1ms.
+  */
+  
+  millisekunden++;
+  if(millisekunden = 1000)
+  {
+	  sekunden++;
+  }
+  
+  
+  
+}
