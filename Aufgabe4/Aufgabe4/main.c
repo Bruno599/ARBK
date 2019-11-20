45d2969362b47db0e16cf4462f8254bf02c7982e
Dominik <bruno481995@gmail.com>
2019-11-20 10:04:15 +0100

    prak 4

---
+++ Aufgabe4/Aufgabe4/main.c
@@ -0,0 +1,18 @@
+/*
+ * Aufgabe4.c
+ *
+ * Created: 19.11.2019 16:14:07
+ * Author : bruno
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
